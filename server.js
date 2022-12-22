408628d8553eebb37056b4569d284bf3312de267
Glitch (glitch-blank-node) <none>
2022-12-22 21:17:53 +0000

    🕌🐬 Checkpoint ./server.js:48131217/75

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/",(req, res)=> {
+  res.send("hello user");
+})
+
 app.use(express.json());
 app.use(express.static("public"));
 
