c5b6eef6bbcd5107cddd19093f7adaf1ff976ff7
Glitch (glitch-blank-node) <none>
2022-12-22 21:29:52 +0000

    🍝🎲 Checkpoint ./server.js:48131217/682 ./package.json:48131217/2408

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.get("/",(req, res)=> {
+app.get("/",(req, res)=>{
   res.send("hello user");
-})
+});
 
-app.use(express.json());
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+//app.use(express.json());
+//app.use(express.static("public"));
 
 // Routes go here
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 4000, ()=>console.log("server on " + process.env.PORT));
