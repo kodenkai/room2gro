f960baeb1ac2a9f4ffb7971b23424735b2962a9f
Glitch (glitch-blank-node) <none>
2022-12-23 17:18:22 +0000

    📫🙍 Checkpoint ./routes/items.js copy:48131217/2109 ./routes/items.js:48131217/914

---
+++ routes/items.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const List = require('../models/item')
+
+//all items
+router.get('/', async (req,res) => { 
+  
+})
+
+// New item
+router.get('/new', (req,res) =>{
+
+})
+  //add item
+router.post('/', async (req,res) => {
+
+})
+
+
+module.exports = router
