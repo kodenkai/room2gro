ca979e2072006e0c1b78859300d0dd7953680c8d
Glitch (glitch-blank-node) <none>
2022-12-23 01:38:27 +0000

    👙🔔 Checkpoint ./passport-config.js:48131217/650

--- passport-config.js
+++ passport-config.js
@@ -1,7 +1,26 @@
 const localStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt');
 
-function authentication
 
 function initialize(passport) {
-  passport.use(new localStrategy({usernaneField: 'email'}), authentication)
+  const authentication = (email, password, done) => {
+    const user = getUserByEmail(email);
+    if (user== null) {
+      return done(null, false, {message: 'No user with that email'});
+    }
+    
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        
+      } else {
+        return done(null, false, {message: 'No user with that email'});
+      }
+    } catch {
+      
+    }
+  }
+  passport.use(new localStrategy({usernaneField: 'email'}), authentication);
+  
+  passport.serializeUser((user, done) => {});
+  passport.deserializeUser((id, done) => {});
 }
